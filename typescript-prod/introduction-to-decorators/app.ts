1bf136ae07f7df8c17361bf58f66625295c3a6b2
loxt <emannuelloxt@hotmail.com>
2020-07-21 18:26:48 -0400

    feat: introduction to decorators

---
+++ typescript-prod/introduction-to-decorators/app.ts
@@ -0,0 +1,12 @@
+function log(target, key, descriptor) {
+  console.log(`${key} was called`)
+}
+
+class Calculator {
+  // Using the decorator @log
+
+  @log
+  square(n: number) {
+    return n * n;
+  }
+}
