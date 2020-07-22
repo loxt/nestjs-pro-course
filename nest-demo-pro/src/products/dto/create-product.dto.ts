28cd83d2877a6ddc6b0b639cf06f48edd939b802
loxt <emannuelloxt@hotmail.com>
2020-07-22 09:52:19 -0400

    feat: add validations using ValidationPipe

--- nest-demo-pro/src/products/dto/create-product.dto.ts
+++ nest-demo-pro/src/products/dto/create-product.dto.ts
@@ -1,6 +1,19 @@
+import { IsInt, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
 export class CreateProductDTO {
+  @IsString()
+  @IsNotEmpty()
   readonly id: string;
+
+  @IsString()
+  @IsNotEmpty()
   readonly name: string;
+
+  @IsNotEmpty()
+  @IsInt()
   readonly qty: number;
+
+  @IsNotEmpty()
+  @IsNumber()
   readonly price: number;
 }
