212afcc414457b8e7572c0258c7677a52e78fbcc
loxt <emannuelloxt@hotmail.com>
2020-07-22 16:41:34 -0400

    feat: connect nestjs to mysql using typeorm

--- nest-demo-pro/src/app.module.ts
+++ nest-demo-pro/src/app.module.ts
@@ -1,24 +1,41 @@
-import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsController } from './products/products.controller';
 import { ProductsService } from './products/products.service';
-import { LoggerMiddleware } from './common/middlewares/logger.middleware';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductsModule } from './products/products.module';
+import { Connection } from 'typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'nest-db',
+      entities: [`${__dirname}/**/*.entity{.ts,.js}`],
+      synchronize: true,
+    }),
+    ProductsModule,
+  ],
   controllers: [AppController, ProductsController],
   providers: [AppService, ProductsService],
 })
 export class AppModule {
-  configure(consumer: MiddlewareConsumer) {
-    // consumer.apply(LoggerMiddleware).forRoutes(ProductsController);
-    // consumer
-    //   .apply(LoggerMiddleware)
-    //   .forRoutes({ path: 'products', method: RequestMethod.GET });
-    consumer
-      .apply(LoggerMiddleware)
-      .exclude({ path: 'products', method: RequestMethod.GET })
-      .forRoutes(ProductsController);
+  constructor(private readonly connection: Connection) {
+    console.log(`connection status ${connection.isConnected}`);
   }
+  // configure(consumer: MiddlewareConsumer) {
+  // consumer.apply(LoggerMiddleware).forRoutes(ProductsController);
+  // consumer
+  //   .apply(LoggerMiddleware)
+  //   .forRoutes({ path: 'products', method: RequestMethod.GET });
+  // consumer
+  //   .apply(LoggerMiddleware)
+  //   .exclude({ path: 'products', method: RequestMethod.GET })
+  //   .forRoutes(ProductsController);
+  // }
 }
