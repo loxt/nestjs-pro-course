abfd4fcc40d47be7400bf929704417f2153d090c
loxt <emannuelloxt@hotmail.com>
2020-07-30 13:10:09 -0400

    fix: removed graphql directory

--- graphql/src/author/author.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AuthorService } from './author.service';
-import { AuthorResolver } from './author.resolver';
-
-@Module({
-  providers: [AuthorService, AuthorResolver]
-})
-export class AuthorModule {}
--- nestjs-with-mongo-prod/src/main.ts
+++ nestjs-with-mongo-prod/src/main.ts
@@ -13,11 +13,10 @@ async function bootstrap() {
     .setDescription('JWT Authentication demo with MongoDB')
     .setVersion('1.0.0')
     .addTag('Users')
-    .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, options);
-  // setup  the swagger module
+  // setup the swagger module
   SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
