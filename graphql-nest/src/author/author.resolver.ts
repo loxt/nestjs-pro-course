f0e00b1e9e14da3cf9338d5997eb944261931a53
loxt <emannuelloxt@hotmail.com>
2020-07-31 00:14:48 -0400

    feat: resolve parent object in resolvers

--- graphql-nest/src/author/author.resolver.ts
+++ graphql-nest/src/author/author.resolver.ts
@@ -1,4 +1,11 @@
-import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import {
+  Args,
+  Mutation,
+  Parent,
+  Query,
+  ResolveProperty,
+  Resolver,
+} from '@nestjs/graphql';
 import { AuthorService } from './author.service';
 import { Author } from './author.interface';
 import { CreateAuthorDTO } from './dto/create-author.dto';
@@ -34,5 +41,19 @@ export class AuthorResolver {
   @Query()
   async author(@Args('id') id: string): Promise<Author> {
     return await this.authorService.findOne(id);
+  }
+
+  @ResolveProperty()
+  posts(@Parent() author) {
+    const { id } = author;
+    // fetch all the posts from mongodb post collection by authorId
+    return [{ id: '1', title: 'LEARN NODE' }];
+  }
+
+  @ResolveProperty()
+  address(@Parent() author) {
+    const { id } = author;
+    // fetch the author address on the based on author id
+    return [{ id: '1', zipCode: '13123', phone: '939123812', city: 'ivi' }];
   }
 }
