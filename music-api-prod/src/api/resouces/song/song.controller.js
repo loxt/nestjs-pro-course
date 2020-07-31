cdd050c913661c8747e45d4ecb9a61a4ac4c9cf8
loxt <emannuelloxt@hotmail.com>
2020-07-31 00:41:54 -0400

    feat(music-api): joi validation

--- music-api-prod/src/api/resouces/song/song.controller.js
+++ music-api-prod/src/api/resouces/song/song.controller.js
@@ -1,5 +1,18 @@
+import Joi from 'joi';
+
 export default {
   create(req, res) {
-    return res.json({ json: 'TODO: SONG CREATED' });
+    const schema = Joi.object().keys({
+      title: Joi.string().required(),
+      url: Joi.string().required(),
+      rating: Joi.number().integer().min(0).max(5).optional()
+    });
+    const { value, error } = schema.validate(req.body);
+
+    if (error && error.details) {
+      return res.status(400).json(error);
+    }
+
+    return res.json(value);
   }
 }
