cda65e1d09800d93024bc3fa7a48a353072a390c
loxt <emannuelloxt@hotmail.com>
2020-08-17 20:42:44 -0400

    feat: creat a new record endpoint

--- music-api-prod/src/api/resouces/song/song.controller.js
+++ music-api-prod/src/api/resouces/song/song.controller.js
@@ -1,18 +1,25 @@
 import Joi from 'joi';
+import Song from './song.model';
 
 export default {
-  create(req, res) {
-    const schema = Joi.object().keys({
-      title: Joi.string().required(),
-      url: Joi.string().required(),
-      rating: Joi.number().integer().min(0).max(5).optional()
-    });
-    const { value, error } = schema.validate(req.body);
+  async create(req, res) {
+    try {
+      const schema = Joi.object().keys({
+        title: Joi.string().required(),
+        url: Joi.string().required(),
+        rating: Joi.number().integer().min(0).max(5).optional()
+      });
+      const { value, error } = schema.validate(req.body);
 
-    if (error && error.details) {
-      return res.status(400).json(error);
+      if (error && error.details) {
+        return res.status(400).json(error);
+      }
+
+      const song = await Song.create(value);
+      return res.json(song);
+    } catch (err) {
+      console.error(err);
+      return res.status(500).send(err);
     }
-
-    return res.json(value);
   }
 }
